5c76a81156c3ffcbbd1838467266d4711ea21d02
Kevin Fivé <kevin.five@kunstmaan.be>
2017-10-13 10:55:33 +0200

    u

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import data from './data/about.json';
 
+const services = data.services.list;
+
 class About extends Component {
     render () {
         return (
-            <h1>About</h1>
+            <div>
+                <section className="page-section" id="section-about">
+                    <h2 className="page-section__header">{data.title}</h2>
+                    <p>{data.intro}</p>
+                </section>
+                <section className="page-section background--theme-secondary">
+                    <h2 className="page-section__header">
+                        {data.services.title}
+                    </h2>
+                    <section className="services">
+                        
+                    </section>
+                </section>
+            </div>
         )
     }
 }
